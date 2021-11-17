3255950c350b73b4ab3b416bd55458909609e352
sbvhev <rohannepal200@gmail.com>
2021-11-17 11:51:12 -0500

    feat: initial commit

---
+++ constants.js
@@ -0,0 +1,14 @@
+const { ethers } = require("ethers");
+
+const bscMainnet = 'https://bsc-dataseed.binance.org';
+const bscTestnet = 'https://rinkeby.infura.io/v3/48739d1e1ba44b37aa68602953072cbc';
+
+const bnbPredictionMainnet = '0xC8bD92Ba3d77b5dad6b597a603C6eDEd9f2c07f0';
+const bnbPredictionTestnet = '0xC8bD92Ba3d77b5dad6b597a603C6eDEd9f2c07f0';
+
+module.exports = {
+  bscMainnet,
+  bscTestnet,
+  bnbPredictionMainnet,
+  bnbPredictionTestnet
+};
